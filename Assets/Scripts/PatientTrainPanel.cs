a5cb7dd4fa75350390f2dee48cceb612769455a7
Zzzzzsl <369355097@qq.com>
2019-07-21 16:29:53 +0800

    z1.8

--- Assets/Scripts/PatientTrainPanel.cs
+++ Assets/Scripts/PatientTrainPanel.cs
@@ -5,31 +5,11 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PatientTrainPanel : MonoBehaviour {
+public class PatientTrainPanel : MonoBehaviour, IPointerClickHandler {
 
-<<<<<<< HEAD
+
     public void OnPointerClick(PointerEventData eventData)
-=======
-    public GameObject leaphandleft;
-    public GameObject leaphandright;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Debug.Log("Escape");
-            //TODO:Pause and ask if patient needed to quit
-            ClickTrainPauseButton();
-        }
-	}
-
-    public void ClickTestButton()
->>>>>>> abbc849db63064b5dff7c46af4221efc5ea98d8b
     {
         if (eventData.pointerPress.name == "TrainPauseButton")       //如果当前按下的按钮是注册按钮 
         {
@@ -37,10 +17,5 @@ public class PatientTrainPanel : MonoBehaviour {
         }
     }
 
-    public void ClickTrainPauseButton()
-    {
-        Debug.Log("Pause");
-        leaphandleft.SetActive(false);
-        leaphandright.SetActive(false);
-    }
+
 }
